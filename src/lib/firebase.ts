d0ff2a9cb3e7b9afbea169d279fd1b170033b29c
Vishnu V <vishnu.v2704@gmail.com>
2025-08-05 13:35:04 +0000

    modified:   src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFmwrhRs1jClRtoX6IUxR_SoSFghv4Frc",
-  authDomain: "iot-test-39bd6.firebaseapp.com",
-  projectId: "iot-test-39bd6",
-  storageBucket: "iot-test-39bd6.firebasestorage.app",
-  messagingSenderId: "366169098953",
-  appId: "1:366169098953:web:fb6c1d6ad15a6bcb249b18",
-  measurementId: "G-QJLR8TRK0X"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
